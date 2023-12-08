f3a64e22045dff95925c0d30440e59da476dd641
Si-Borg <84985515+Si-Borg@users.noreply.github.com>
2023-12-07 22:01:51 -0500

    Added function that creates a wobble effect based on touch or mouse position

--- js/homeScript.js
+++ js/homeScript.js
@@ -1,7 +1,12 @@
 "use strict";
 
+import { wobbleEffect, mobileWobbleEffect } from "./scripts.js";
+
 window.addEventListener("load", init);
 
 function init() {
-      // TODO: add code or something
+      // Get and add event listener for wobble effect to the profile picture element
+      let profileImg = document.getElementById("profile");
+      profileImg.addEventListener("mousemove", wobbleEffect);
+      profileImg.addEventListener("touchmove", mobileWobbleEffect);
 }
